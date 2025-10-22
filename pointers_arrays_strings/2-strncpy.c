60523e346381ebb7f617a7f9766809ccdb6d1eba
Modi-01 <moudhi.a.almutlaq@gmail.com>
2025-10-22 10:32:07 +0000

    task2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -6,9 +6,9 @@
  * @src: source string
  * @n: maximum number of bytes to copy
  *
- * Description: Behaves like the standard strncpy. If src is shorter than n,
- * the remaining bytes in dest are padded with '\0'. If src length is at
- * least n, no terminating null byte is added.
+ * Description: Behaves like standard strncpy.
+ * If src is shorter than n, pad the rest of dest with '\0'.
+ * If src length is >= n, do not add a terminating '\0'.
  *
  * Return: pointer to dest
  */
@@ -16,9 +16,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
+	/* copy from src to dest while both n not exhausted and src not ended */
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
+	/* if src ended before n, pad remaining bytes with '\0' */
 	for (; i < n; i++)
 		dest[i] = '\0';
 
