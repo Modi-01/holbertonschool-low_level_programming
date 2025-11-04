14e7842544534cd4788567c2b198b48a8900ef03
Modi-01 <moudhi.a.almutlaq@gmail.com>
2025-11-04 15:21:30 +0000

    task0

---
+++ more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+/* Prototype required by the checker */
+void *malloc_checked(unsigned int b);
+
+/* Provided by the checker; you don't push its .c file */
+int _putchar(char c);
+
+#endif /* MAIN_H */
