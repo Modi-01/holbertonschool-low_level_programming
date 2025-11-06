4c756665d91baf8c430226beb82896db73a1469d
Modi-01 <moudhi.a.almutlaq@gmail.com>
2025-11-06 11:25:38 +0000

    task3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,7 +17,26 @@ struct dog
 	char *owner;
 };
 
+/* New type definition for struct dog */
+typedef struct dog dog_t;
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: pointer to name
+ * @age: dog's age
+ * @owner: pointer to owner's name
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to struct dog to print
+ *
+ * Description: Prints the name, age, and owner of a dog.
+ * If any element is NULL, it prints (nil) instead.
+ * If d is NULL, the function prints nothing.
+ */
 void print_dog(struct dog *d);
 
 #endif /* DOG_H */
