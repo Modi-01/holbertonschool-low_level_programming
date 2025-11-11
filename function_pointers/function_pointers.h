4eca18b8c9a286c6b95f87dfa13b341aecd8fd2d
Modi-01 <moudhi.a.almutlaq@gmail.com>
2025-11-11 15:43:52 +0000

    task0

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,15 +3,23 @@
 
 /**
  * File: function_pointers.h
- * Desc: Prototypes for function pointers tasks.
+ * Desc: Prototypes used in the function pointers tasks.
  */
 
+/**
+ * _putchar - Write a character to standard output
+ * @c: Character to print
+ *
+ * Return: 1 on success, or -1 on error with errno set.
+ */
 int _putchar(char c);
 
 /**
- * print_name - prototype for printing a name via a callback
- * @name: pointer to the name string
- * @f: pointer to a function that takes (char *) and returns void
+ * print_name - Call a callback function to print a name
+ * @name: Pointer to the name string
+ * @f: Pointer to a function that takes (char *) and returns void
+ *
+ * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *));
 
