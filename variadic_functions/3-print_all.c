c4f8ccfdc7584f3b89e3e52898c87a8c17c5012f
Modi-01 <moudhi.a.almutlaq@gmail.com>
2025-11-12 12:09:29 +0000

    task3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,9 +3,46 @@
 #include "variadic_functions.h"
 
 /**
+ * print_one - print one argument based on type specifier
+ * @t: type specifier ('c', 'i', 'f', 's')
+ * @ap: variadic list pointer
+ * @sep: in/out current separator to print before the value
+ *
+ * Return: Nothing.
+ */
+static void print_one(char t, va_list *ap, const char **sep)
+{
+	char *s;
+
+	switch (t)
+	{
+	case 'c':
+		printf("%s%c", *sep, (char)va_arg(*ap, int));
+		*sep = ", ";
+		break;
+	case 'i':
+		printf("%s%d", *sep, va_arg(*ap, int));
+		*sep = ", ";
+		break;
+	case 'f':
+		printf("%s%f", *sep, va_arg(*ap, double));
+		*sep = ", ";
+		break;
+	case 's':
+		s = va_arg(*ap, char *);
+		if (s == NULL)
+			s = "(nil)";
+		printf("%s%s", *sep, s);
+		*sep = ", ";
+		break;
+	default:
+		break;
+	}
+}
+
+/**
  * print_all - prints anything according to a format string
- * @format: string of type chars:
- *          'c' char, 'i' int, 'f' float, 's' char* (NULL -> "(nil)")
+ * @format: string of type chars: 'c', 'i', 'f', 's'
  *
  * Return: Nothing.
  */
@@ -13,44 +50,13 @@ void print_all(const char * const format, ...)
 {
 	va_list ap;
 	unsigned int i = 0;
-	char *s;
-	char *sep = "";
-	char t;
+	const char *sep = "";
 
 	va_start(ap, format);
 
 	if (format != NULL)
-	{
 		while (format[i] != '\0')
-		{
-			t = format[i];
-			switch (t)
-			{
-			case 'c':
-				printf("%s%c", sep, (char)va_arg(ap, int));
-				sep = ", ";
-				break;
-			case 'i':
-				printf("%s%d", sep, va_arg(ap, int));
-				sep = ", ";
-				break;
-			case 'f':
-				printf("%s%f", sep, va_arg(ap, double));
-				sep = ", ";
-				break;
-			case 's':
-				s = va_arg(ap, char *);
-				if (s == NULL)
-					s = "(nil)";
-				printf("%s%s", sep, s);
-				sep = ", ";
-				break;
-			default:
-				break;
-			}
-			i++;
-		}
-	}
+			print_one(format[i++], &ap, &sep);
 
 	printf("\n");
 	va_end(ap);
